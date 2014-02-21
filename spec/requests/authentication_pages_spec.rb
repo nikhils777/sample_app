d041c73b6b3a84e864045f326771901fef0be250
nikhils777 <nsharmatennis@gmail.com>
2014-02-21 16:26:59 -0500

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -44,6 +44,18 @@ describe "Authentication" do
           click_button "Sign in"
         end
       end
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
